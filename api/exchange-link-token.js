2f519b200f1198ad3f1e1a974b6bb17b8a254a99
Lakeman251 <grayfullberg@gmail.com>
2025-10-18 14:39:53 +0500

    Update exchange-link-token.js

--- api/exchange-link-token.js
+++ api/exchange-link-token.js
@@ -39,6 +39,8 @@ function makeSession(tg_id) {
 module.exports = (req, res) => {
   const ORIGIN = process.env.CORS_ORIGIN || "*";
   res.setHeader("Access-Control-Allow-Origin", ORIGIN);
+  res.setHeader("Vary", "Origin"); // ← новое
+  res.setHeader("Access-Control-Allow-Credentials", "true"); // ← новое
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   if (req.method === "OPTIONS") return res.status(204).end();
